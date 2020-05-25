373df6bd30fbc6c4025dc26dfc4c9ba199eb319e
NeoTRAN <neo_tran@outlook.com>
2020-05-25 14:48:17 -0500

    New Script API

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 users = [
-    { "id": 1, "name": "Dan",  "email": "dan@hotmail.com", "status": True },
-    { "id": 2, "name": "Kaku", "email": "kaku@gmail.com" , "status": False },
-    { "id": 3, "name": "Carl", "email": "carl@yahoo.com" , "status": True },
+    {"id": 1, "name": "Dan", "email": "dan@hotmail.com", "status": True},
+    {"id": 2, "name": "Kaku", "email": "kaku@gmail.com", "status": False},
+    {"id": 3, "name": "Carl", "email": "carl@yahoo.com", "status": True},
 ]
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data import users
 
 app = Flask(__name__)
 
-# ********************* Get Data *********************
+# Get
 @app.route('/', methods=['GET'])
 def get_users():
     return jsonify(users)
@@ -11,33 +11,21 @@ def get_users():
 @app.route('/<int:id_user>', methods=['GET'])
 def get_user(id_user):
     return jsonify(
-        [ i for i in users if i['id'] == id_user]
+        [i for i in users if i['id'] == id_user]
     )
-
-# ********************* Insert Data *********************
+# Post
 @app.route('/', methods=['POST'])
 def add_user():
     new_user = {
-        "id"     : request.json['id'],
-        "name"   : request.json['name'],
-        "email"  : request.json['email'],
-        "status" : request.json['status'] 
+        "id": request.json['id'],
+        "name": request.json['name'],
+        "email": request.json['email'],
+        "status": request.json['status']
     }
+
     users.append(new_user)
-    return jsonify({"message": "Product Added Succesfully", "New Users": users})
-
-# ********************* Delete Data *********************
-@app.route('/<int:id_user>', methods=['DELETE'])
-def delete_user(id_user):
-    obj_user = [ i for i in users if i['id'] == id_user]
-    if len(obj_user) > 0:
-        users.remove(obj_user[0])
-        return jsonify({
-            "message": 'Product Deleted',
-            'users': users
-        })
-
-# ********************* Update Data *********************
+    return jsonify({"message": "User added Succesfully", "New Users": users})
+# Put
 @app.route('/<int:id_user>', methods=['PUT'])
 def update_user(id_user):
     obj_user = [i for i in users if i['id'] == id_user]
@@ -46,21 +34,36 @@ def update_user(id_user):
         users.remove(obj_user[0])
 
         obj_user = {
-            "id":     request.json['id'],
-            "name":   request.json['name'],
-            "email":  request.json['email'],
-            "status": request.json['status'] 
+            "id": request.json["id"],
+            "name": request.json['name'],
+            "email": request.json['email'],
+            "status": request.json['status']
         }
 
         users.append(obj_user)
 
         return jsonify({
-            'message': 'Product Updated',
+            'message': 'User updated',
             'users': obj_user
         })
+    
+    return "User not found"
+
+# Delete
+@app.route('/<int:id_user>', methods=['DELETE'])
+def delete_user(id_user):
+    obj_user = [i for i in users if i['id'] == id_user]
+
+    if len(obj_user) > 0:
+        users.remove(obj_user[0])
+        return jsonify({
+            "message": 'User Deleted',
+            'users': users
+        })
+    return 'User not found'
 
 def main():
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=8080)
 
 if __name__ == '__main__':
     main()
